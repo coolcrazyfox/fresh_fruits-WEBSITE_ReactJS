fe186c24f06952239241526d03448ae75d155503
Andrei Malets <coolviktori@yandex.by>
2024-08-14 20:31:53 +0300

    changes NavBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import { FaLeaf } from "react-icons/fa";
 
+const menuNavBar = [
+  { id: 1, name: "Home", link: "/" },
+  { id: 2, name: "Products", link: "/products" },
+  { id: 3, name: "Shop", link: "/shop" },
+  { id: 4, name: "Contacts", link: "/contacts" },
+  { id: 5, name: "About", link: "/about" },
+];
+
 const Navbar = () => {
   return (
     <nav>
       <div className="container">
-        <div className="text-2xl flex items-center gap-2">
-          <p className="text-primary">Fruit</p>
-          <p className="text-secondary">Store</p>
+        <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+          <p className="text-secondary">Fruit</p>
+          <p className="text-primary">Store</p>
           <FaLeaf className="text-green-400 " />
+        </div>
+        <div>
+          {menuNavBar.map((item, index) => {
+            return <div key={item.id}>{item.name}</div>;
+          })}
         </div>
       </div>
     </nav>
