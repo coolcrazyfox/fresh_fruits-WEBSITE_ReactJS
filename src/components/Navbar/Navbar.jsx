6b688a641f8d869b6c92ee798ef8189b83bb2829
Andrei Malets <coolviktori@yandex.by>
2024-08-14 20:13:54 +0300

    changes NavBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,11 @@ const Navbar = () => {
   return (
     <nav>
       <div className="container">
-        <p>Fruit</p>
-        <p>Store</p>
-        <FaLeaf />
+        <div className="text-2xl flex items-center gap-2">
+          <p className="text-primary">Fruit</p>
+          <p className="text-secondary">Store</p>
+          <FaLeaf className="text-green-400 " />
+        </div>
       </div>
     </nav>
   );
