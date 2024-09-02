6f0e599088580105b0e8be40b590aa0575702630
Andrei Malets <coolviktori@yandex.by>
2024-09-02 23:42:03 +0300

    changes Menus

--- src/components/Menus/SecondMenus.jsx
+++ src/components/Menus/SecondMenus.jsx
@@ -5,37 +5,37 @@ import { BiDollar } from "react-icons/bi";
 import img1 from "../../assets/fruits/banana.png";
 import img2 from "../../assets/fruits/cherries.png";
 import img3 from "../../assets/fruits/orange.png";
-import img4 from "../../assets/fruits/apple.png";
+import img4 from "../../assets/fruits/watermelon.png";
 const MenuData = [
   {
     id: 1,
-    name: "Fresh Bananas",
-    link: "/bananas",
-    price: 1.99,
+    name: "Fresh Pineapples",
+    link: "/pineapples",
+    price: 2.99,
     img: img1,
     delay: 0.3,
   },
   {
     id: 2,
-    name: "Fresh Cherries",
-    link: "/cherries",
-    price: 3.99,
+    name: "Fresh Strawberry",
+    link: "/strawberry",
+    price: 5.99,
     img: img2,
     delay: 0.6,
   },
   {
     id: 3,
-    name: "Fresh Oranges",
-    link: "/oranges",
-    price: 2.99,
+    name: "Fresh Grape",
+    link: "/grape",
+    price: 6.99,
     img: img3,
     delay: 0.9,
   },
   {
     id: 4,
-    name: "Fresh Apples",
-    link: "/apples",
-    price: 1.99,
+    name: "Fresh Watermelon",
+    link: "/watermelon",
+    price: 2.99,
     img: img4,
     delay: 1.2,
   },
