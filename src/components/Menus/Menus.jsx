bbc69e79e05f2548e5bacb4ee3a184bde7565d98
Andrei Malets <coolviktori@yandex.by>
2024-09-02 21:27:20 +0300

    create Menus

--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import img1 from "../../assets/fruits/avocado.png";
+import img2 from "../../assets/fruits/cherry.png";
+import img3 from "../../assets/fruits/orange.png";
+import img4 from "../../assets/fruits/apple.png";
+
+const MenuData = [
+  { id: 1, name: "Fresh Avocado", link: "/avocado", price: 5.99, img: img1 },
+  { id: 2, name: "Fresh Cherries", link: "/cherries", price: 4.99, img: img2 },
+  { id: 3, name: "Fresh Oranges", link: "/oranges", price: 2.99, img: img3 },
+  { id: 4, name: "Fresh Apples", link: "/apples", price: 1.99, img: img4 },
+];
+
 const Menus = () => {
-  return <div>Menus</div>;
+  return (
+    <section>
+      <div className="container">
+        <h1 className="font-bold text-2xl text-left pb-10 cursor-pointer">
+          Our Menu
+        </h1>
+        <div>
+          {MenuData.map((m, index) => {
+            <div key={m.id}>{m.name}</div>;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Menus;
