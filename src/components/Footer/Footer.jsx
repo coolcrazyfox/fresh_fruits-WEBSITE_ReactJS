03c5fca5bdc88d2325a835257ae304234fcfab9f
Andrei Malets <coolviktori@yandex.by>
2024-09-08 23:03:35 +0300

    changes BannerThird

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { FadeLeft } from "./../../utility/animation";
+import { FaInstagram, FaTwitter, FaLeaf, FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container"></div>
+    </footer>
+  );
 };
 
 export default Footer;
