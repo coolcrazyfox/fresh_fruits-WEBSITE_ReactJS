562b08ae905dc9cb43c02c6f74d4dd17232c39f2
Andrei Malets <coolviktori@yandex.by>
2024-09-08 22:20:02 +0300

    changes BannerSecond

--- src/components/Banners/BannerSecond.jsx
+++ src/components/Banners/BannerSecond.jsx
@@ -73,9 +73,9 @@ const BannerSecond = () => {
         </div>
         <div className="flex justify-center items-center">
           <motion.img
-            initial={{ opacity: 0, scale: 0.5 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ type: "string", stiffness: 100, delay: 0.2 }}
+            initial={{ opacity: 0, x: 200, rotate: 75 }}
+            whileInView={{ opacity: 1, x: 0, rotate: 0 }}
+            transition={{ duration: 1, delay: 0.2 }}
             viewport={{ once: true }}
             src={BannerImg}
             alt="fruits_banner"
