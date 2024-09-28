7956f8edf23e90c525d10b35c61eecefa43ff9e6
Andrei Malets <coolviktori@yandex.by>
2024-09-28 22:50:18 +0300

    changes App

---
+++ src/routes/RouterRoot.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Hero from "../pages/Hero";
+import About from "../pages/About";
+import Explore from "../pages/Explore";
+import Gallery from "../pages/Gallery";
+import Contact from "../pages/Contact";
+import Error404 from "../pages/Contact";
+
+export const PATH = {
+  HERO: "/",
+  ABOUT: "/about",
+  EXPLORE: "/explore",
+  GALLERY: "/gallery",
+  CONTACT: "/contact",
+  ERROR404: "*",
+};
+const RouterRoot = () => {
+  return (
+    <>
+      <Routes>
+        <Route path={PATH.HERO} element={<Hero />} />
+        <Route path={PATH.ABOUT} element={<About />} />
+        <Route path={PATH.EXPLORE} element={<Explore />} />
+        <Route path={PATH.GALLERY} element={<Gallery />} />
+        <Route path={PATH.CONTACT} element={<Contact />} />
+        <Route path={PATH.ERROR404} element={<Error404 />} />
+      </Routes>
+    </>
+  );
+};
+
+export default RouterRoot;
